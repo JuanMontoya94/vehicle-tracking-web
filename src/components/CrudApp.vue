ed2b77aeb9bea305e8e0e03b2ee531f16c07af9f
Juan Trabajo <juand.montoyam@uqvirtual.com.co>
2022-05-16 17:31:32 -0500

    botones

--- src/components/CrudApp.vue
+++ src/components/CrudApp.vue
@@ -8,17 +8,23 @@
         <Column field="date" header="Fecha"></Column>
         <Column field="status" header="Estado"></Column>
         <Column field="vehicle.plate" header="Placa"></Column>
+        <Column field="Button" header="Acción">
+        <template #body>
+      <Button type="button" label="Editar" class="p-button-raised p-button-primary" style="margin: 15px"  ></Button>
+      <Button type="button" label="Eliminar" class="p-button-raised p-button-secondary" style="margin: 15px" ></Button>
+    </template>
+        </Column>
       </DataTable>
 
     </Panel>
     <Dialog header="Crear Persona" :visible.sync="displayModal" :modal="true">
       <span class="p-float-label">
-       <Dropdown v-model="selectedEmployee" :options="employees" optionLabel="name" placeholder="Codigo empleado" style="width: 80%"  />
+       <Dropdown v-model="appointment.employee" :options="employees" optionLabel="id" placeholder="Codigo empleado" style="width: 80%"  />
       </span>
       <br />
       <br />
       <span class="p-float-label">
-        <InputText id="plate" type="text" v-model="appointment.date" style="width: 100%" />
+        <InputText id="plate" type="text" v-model="appointment.vehicle.plate" style="width: 100%" />
         <label for="plate">Placa vehiculo</label>
       </span>
       <br />
@@ -28,6 +34,8 @@
         <Button label="Cancelar" icon="pi pi-times" @click="closeModal" class="p-button-secondary" />
       </template>
     </Dialog>
+
+    
   </div>
 </template>
 
@@ -39,19 +47,21 @@ export default {
   data() {
     return {
       selectedEmployee:null,
+      
       employees:[
-        {name: '1', code: '1'}
+        {id: '1'}
       ]
       ,
       appointments: null,
       appointment: {
-        id: null,
-        date: null,
-        status: null,
+        employee:{
+          id:null
+        },
         vehicle:{
           plate : null
         }
       },
+      
       items: [
         {
           label: "Nuevo",
@@ -97,16 +107,17 @@ export default {
     },
     save() {
       this.personaService.save(this.appointment).then(data => {
-        if (data.status === 200) {
+        console.log(data)
+         if (data.status === 200) {
           this.displayModal = false;
           this.appointment = {
-            employee: null,
-            vehicle:{
-              plate : null
+             employee: null,
+             vehicle:{
+               plate : null
             }
-          };
-          this.getAll();
-        }
+           };
+           this.getAll();
+         }
       });
     },
     closeModal() {
--- src/service/PersonaService.js
+++ src/service/PersonaService.js
@@ -4,12 +4,14 @@ import axios from 'axios';
 export default class PersonaService {
 
     url = "http://localhost:8080/appointment";
+    //url = "https://vehicle-tracking-uniquindio.herokuapp.com/appointment";
 
     getAll() {
         return axios.get(this.url);
     }
 
     save(persona) {
+        console.log(persona);
         return axios.post(this.url, persona)
     }
 }
