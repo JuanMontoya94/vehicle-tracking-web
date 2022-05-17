3cba61faec94d343b6ce71c9df589395e24d12e9
Juan Trabajo <juand.montoyam@uqvirtual.com.co>
2022-05-17 11:50:10 -0500

    make it better

--- src/service/PersonaService.js
+++ src/service/PersonaService.js
@@ -2,9 +2,8 @@ import axios from 'axios';
 
 
 export default class PersonaService {
-
-    url = "http://localhost:8080/appointment/";
-    //url = "https://vehicle-tracking-uniquindio.herokuapp.com/appointment";
+    
+    url = "https://vehicle-tracking-uniquindio.herokuapp.com/appointment";
 
     getAll() {
         return axios.get(this.url);
